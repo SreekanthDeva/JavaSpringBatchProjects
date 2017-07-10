7fd4791b6d1d9659983e6da883f2b1685ecac82a
SreekanthDeva <sreekanthdeva@gmail.com>
2017-07-10 07:06:49 +0530

    Add files via upload

---
+++ raw_report.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Community v12.4.0 (64 bit)
+MySQL - 5.7.13-log : Database - phani
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`phani` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `phani`;
+
+/*Table structure for table `raw_report` */
+
+DROP TABLE IF EXISTS `raw_report`;
+
+CREATE TABLE `raw_report` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `date` varchar(45) DEFAULT NULL,
+  `impressions` varchar(45) DEFAULT NULL,
+  `clicks` varchar(45) DEFAULT NULL,
+  `earning` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
